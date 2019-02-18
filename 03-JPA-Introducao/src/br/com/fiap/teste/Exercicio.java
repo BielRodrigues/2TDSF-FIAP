9a8e45356beba1017bf7a1fe0c507e2668910c56
Gabriel Rodrigues <gabrielm1212@gmail.com>
2019-02-18 20:09:23 -0300

    Finalizando o exercício do CRUD.

--- 03-JPA-Introducao/src/br/com/fiap/teste/Exercicio.java
+++ 03-JPA-Introducao/src/br/com/fiap/teste/Exercicio.java
@@ -11,24 +11,35 @@ import br.com.fiap.entity.Cliente;
 import br.com.fiap.entity.Estado;
 import br.com.fiap.entity.Produto;
 
-public class Exercicio 
+public class Exercicio {
 	public static void main(String[] args) {
 		//Realizar o CRUD para o produto
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("CLIENTE_ORACLE");
 		EntityManager em = fabrica.createEntityManager();
 		
-		em.getTransaction().begin();
 		
 		//Cadastrar
-		Produto produto = new Produto("Chocolate", 2, Estado.NOVO, new GregorianCalendar(2019, Calendar.DECEMBER, 5), new GregorianCalendar(2019, Calendar.JANUARY, 22), 8, null);
-		em.persist(produto);
-		//Pesquisar
+//		Produto produto = new Produto("Chocolate", 2, Estado.NOVO, new GregorianCalendar(2019, Calendar.DECEMBER, 5), new GregorianCalendar(2019, Calendar.JANUARY, 22), 8, null);
+//		em.persist(produto);
+		
+//		//Pesquisar
+//		Produto produtoPesquisar = em.find(Produto.class, 1);
+//		System.out.println(produtoPesquisar.getNome());
 		
 		//Atualizar
+//		Produto produtoAtualiza = em.find(Produto.class, 1);
+//		produtoAtualiza.setNome("Coca-cola");
+		
+		Produto produto = new Produto(1 ,"Vinho", 2, Estado.NOVO, new GregorianCalendar(2019, Calendar.DECEMBER, 5), new GregorianCalendar(2019, Calendar.JANUARY, 22), 8, null);
+		em.merge(produto);
 		
 		//Remover
+		// Foi deletado o produto com o ID 2
+//		Produto produtoDeletar = em.find(Produto.class, 2);
+//		em.remove(produtoDeletar);
 		
-	
+		
+		em.getTransaction().begin();
 		em.getTransaction().commit();
 		em.close();
 		fabrica.close();
